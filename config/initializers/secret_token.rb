57cb6b8a9b0b6d43afb490265a6cd999d6be204b
kat <kathleenlogmao@gmail.com>
2014-07-17 16:58:46 +0800

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AteneoBudgetWebsite::Application.config.secret_key_base = 'c0ef9a790626a4d77a149b36a0a899a77521c88469f9ccd33a21ddf8696dfaadfabe7be64a61f40282aa192d1759277c4b794dc54b8c9738b1b297b146ee327d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AteneoBudgetWebsite::Application.config.session_store :cookie_store, key: '_ateneo-budget-website_session'
