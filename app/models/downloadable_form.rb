c5ebbc6299b3a11c0a4738e63d4312d720c0c58d
Raphael Alampay <raphael.alampay@gmail.com>
2014-11-27 16:38:04 +0800

    enable ms word upload

--- app/models/downloadable_form.rb
+++ app/models/downloadable_form.rb
@@ -7,7 +7,7 @@ class DownloadableForm < ActiveRecord::Base
 
   has_attached_file :file
   #validates_attachment_file_name :file, :matches => [/doc\Z/, /xlsx?g\Z/, /xls\Z/, /docx\Z/]
-  validates_attachment_content_type :file, :content_type => %w(application/xml application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
+  validates_attachment_content_type :file, :content_type => %w(application/msword application/xml application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
 
   def file_attached?
     self.file.file?
